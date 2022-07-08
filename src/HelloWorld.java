26ed37e76d8b987ebcbda3fb9dc8730bb1edffc8
KaTatiana <badinat@mail.ru>
2022-07-08 23:16:46 +0300

    Initial commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,10 @@
+//package ru.skypro;
+
+import static java.lang.System.*;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+
+        out.println("Hello World!");
+    }
+}
