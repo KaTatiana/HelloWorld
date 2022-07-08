4cc3a3e4d62a0b8a3ca23c89f3d13fff0a3b95e9
KaTatiana <badinat@mail.ru>
2022-07-08 23:27:02 +0300

    Initial commit

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -5,6 +5,6 @@ import static java.lang.System.*;
 public class HelloWorld {
     public static void main(String[] args) {
 
-        out.println("Hello World!");
+        out.println("Hello World!"); //вывод строки
     }
 }
